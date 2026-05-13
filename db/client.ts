d45165a09b3ebc8dacc1304ae2208a56cba5def9
Jody LeCompte <jody@jodylecompte.com>
2026-05-13 16:16:41 -0500

    Changes to db client for neon

--- db/client.ts
+++ db/client.ts
@@ -2,8 +2,12 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
 import * as schema from './schema'
 
+const url = process.env.DATABASE_URL!
+const isNeon = url.includes('neon.tech')
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: url,
+  ssl: isNeon ? { rejectUnauthorized: false } : false,
 })
 
 export const db = drizzle(pool, { schema })
